f56310cf2da76e23f0baa290698e39cdf32c1a2a
Mansoor Khan <mansoorkh1098@gmail.com>
2023-01-22 06:36:49 +0530

    delete duplicate file

--- src copy/Components/TodoForm/Input.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Input = React.forwardRef((props, ref) => {
-  //   return <input type="text" onChange={nameHandler} />;
-  return <input ref={ref} {...props.input} />;
-});
-
-export default Input;
